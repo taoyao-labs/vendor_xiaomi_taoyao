17a6222980f57dd396cbcf5ce6110c2b819024af
Ammar Bahtiar <ammarbahtiarasli@gmail.com>
2025-10-28 23:37:24 +0000

    taoyao: Adding IR blobs

--- taoyao-vendor.mk
+++ taoyao-vendor.mk
@@ -829,6 +829,7 @@ PRODUCT_PACKAGES += \
     camera.qcom \
     camera.xiaomi \
     com.qti.chi.override \
+    consumerir.lahaina \
     displayfeature.default \
     fingerprint.goodix_fod.default \
     memtrack.default \
