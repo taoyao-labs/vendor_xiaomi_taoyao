faeeb26dad25e67c043e6b2e40d7a57afd9e5c6a
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2026-06-27 12:18:09 +0000

    taoyao: Update Android.mk and BoardConfigVendor.mk

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),taoyao)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,bae6fb7b6b5cc1a52787c9d4c12cc9abadaa86da)
+$(call add-radio-file-sha1-checked,radio/aop.img,c1a4101d07251b5d566faa2058a8f8c3350368fd)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,caf43eb0854a0bd30e12d9b80f69cdd906e117d9)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,963a974f8b1b7b7558654b3fb497b3d874ff939d)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,2806fbe70d66677c371988d1ef6d35a1241edf8f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,d3fdbfa43b8686991d86c6679c3a87b07a11b368)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,4710e36e66ef15a84e3e08dcc97699d4030d72d7)
+$(call add-radio-file-sha1-checked,radio/hyp.img,b1f3a786e2db3bb89112931165944774489dc96d)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,2560e018a0941dd0bce5dc08c350eb77c549aafc)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,86978687ccb415793a304386efea61f0f80ebd3e)
+$(call add-radio-file-sha1-checked,radio/modem.img,7fefa25b3b1b56442ede42c4fdcdfc618af8c9c5)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,19d688dc8d734b39cc7b15040f6b2174e67723bf)
+$(call add-radio-file-sha1-checked,radio/shrm.img,768321acfbe34bd175f685103d45d5ad484c1fcd)
+$(call add-radio-file-sha1-checked,radio/tz.img,a4b0525bbd5c3fef00074df0d2f417303282ed30)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,c984577d219c18faa4e647799f1c9a3ff9436dc3)
+$(call add-radio-file-sha1-checked,radio/xbl.img,5deb21a81bf2f78c057789041f5810fad8393153)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,789682a270f56dcb83a925d5ec89fde0b8ebc07a)
+
+endif
